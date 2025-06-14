5d7b637306936e13f50f6852d9aeda5eb0c4e22a
hottabuch1987 <varvar1987a@mail.ru>
2025-06-14 14:30:10 +0300

    подогнал размеры

--- offer/views.py
+++ offer/views.py
@@ -15,7 +15,6 @@ from PIL import Image
 import os
 import tempfile
 
-
 def convert_to_pdf(request):
     if request.method != 'POST':
         return redirect('product_list')
@@ -30,14 +29,22 @@ def convert_to_pdf(request):
     pdf.set_auto_page_break(auto=False, margin=0)
     
     # Настройка шрифтов
-  
     pdf.add_font('DejaVu', '', '/Users/hottabuch/PycharmProjects/business_offer/business/static/dejavu-fonts-ttf/ttf/DejaVuSansCondensed-Bold.ttf', uni=True)
-    pdf.add_font
-
 
     background1_path = os.path.join(settings.BASE_DIR, 'img1.jpg')  # Для option1
     background2_path = os.path.join(settings.BASE_DIR, 'img2.jpg')  # Для option2
     
+    # Функция для скругления углов
+    def add_rounded_corners(image, radius=20):
+        from PIL import ImageDraw
+        mask = Image.new('L', image.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.rounded_rectangle([(0, 0), image.size], radius, fill=255)
+        if image.mode != 'RGBA':
+            image = image.convert('RGBA')
+        image.putalpha(mask)
+        return image
+
     with tempfile.TemporaryDirectory() as temp_dir:
         for product_id in selected_ids:
             product = get_object_or_404(Product, id=product_id)
@@ -50,56 +57,85 @@ def convert_to_pdf(request):
             try:
                 # Создаем композицию изображения
                 with Image.open(bg_path) as bg:
+                    bg = bg.convert("RGBA")
                     bg_width, bg_height = bg.size
                     
+                    # Обработка главного изображения
                     with Image.open(product.main_photo.path) as product_img:
-                        # Обработка прозрачности
+                        # Конвертация и скругление углов
                         if product_img.mode == 'RGBA':
-                            white_bg = Image.new('RGB', product_img.size, (255, 255, 255))
-                            white_bg.paste(product_img, (0, 0), product_img)
-                            product_img = white_bg
+                            product_img = add_rounded_corners(product_img)
+                        else:
+                            product_img = product_img.convert('RGBA')
+                            product_img = add_rounded_corners(product_img)
                         
-                        # Различное позиционирование для разных вариантов
+                        # Жесткое задание размеров для разных вариантов
+                        if option == "option1":
+                            # Жесткие размеры: 70% ширины и 50% высоты фона
+                            target_width = int(bg_width * 0.3) + 100
+                            target_height = int(bg_height * 0.8) + 40
+                            
+                            # Растягивание до точных размеров
+                            product_img = product_img.resize((target_width, target_height), Image.LANCZOS)
+                            
+                            # Позиционирование
+                            position = (
+                                (bg_width - target_width) // 2 - 500,  # Центр по X
+                                int(bg_height * 0.07)             # 5% от высоты сверху
+                            )
+                        else:  # option2
+                            # Жесткие размеры: 50% ширины и 70% высоты фона
+                            target_width = int(bg_width * 0.5) + 150
+                            target_height = int(bg_height * 0.57)
+                            
+                            # Растягивание до точных размеров
+                            product_img = product_img.resize((target_width, target_height), Image.LANCZOS)
+                            
+                            position = (
+                                bg_width - target_width - 25,  # Справа с отступом
+                                (bg_height - target_height) - 330  # Центр по Y
+                            )
+                        
+                        # Наложение на фон
+                        bg.paste(product_img, position, product_img)
+                        
+                        # Обработка дополнительных изображений
                         additional_photos = []
                         if option == "option1":
-                            # Для option1: добавляем фото в нижнюю часть
                             if product.additional_photo1 and os.path.exists(product.additional_photo1.path):
                                 additional_photos.append(product.additional_photo1.path)
                             if product.additional_photo2 and os.path.exists(product.additional_photo2.path):
                                 additional_photos.append(product.additional_photo2.path)
                                 
                             if additional_photos:
-                                # Параметры для дополнительных фото
-                                footer_height = int(bg_height * 0.22)  # 15% высоты
-                                footer_y = bg_height - footer_height
-                                total_width = bg_width
-                                num_photos = len(additional_photos)
-                                margin_x = 5  # Отступ по горизонтали
-                                margin_y = 5   # Отступ по вертикали
+                                sidebar_width = int(bg_width * 0.18)
+                                sidebar_x = int(bg_width * 0.38)
+                                content_y = int(bg_height * 0.07)
                                 
-                                # Рассчет максимальной ширины для фото
-                                max_photo_width = (total_width - (num_photos + 1) * margin_x) // num_photos
-                                max_photo_height = footer_height - 2 * margin_y
+                                # Жесткие размеры для дополнительных фото
+                                photo_height = int(bg_height * 0.30) + 115
+                                photo_width = sidebar_width
+                                margin_y = 25
                                 
-                                x_pos = margin_x
+                                y_pos = content_y
                                 for photo_path in additional_photos:
                                     with Image.open(photo_path) as img:
-                                        # Конвертация RGBA в RGB
-                                        if img.mode == 'RGBA':
-                                            white_bg = Image.new('RGB', img.size, (255, 255, 255))
-                                            white_bg.paste(img, mask=img.split()[3])
-                                            img = white_bg
+                                        if img.mode != 'RGBA':
+                                            img = img.convert('RGBA')
+                                        img = add_rounded_corners(img, radius=15)
                                         
-                                        # Масштабирование с сохранением пропорций
-                                        img.thumbnail((max_photo_width, max_photo_height), Image.LANCZOS)
+                                        # Растягивание до точных размеров
+                                        img = img.resize((photo_width, photo_height), Image.LANCZOS)
                                         
                                         # Позиционирование
-                                        y_pos = footer_y + margin_y + (max_photo_height - img.height) // 2
-                                        bg.paste(img, (x_pos, y_pos))
+                                        x_pos = sidebar_x
+                                        bg.paste(img, (x_pos, y_pos), img)
                                         
-                                        x_pos += img.width + margin_x
+                                        y_pos += photo_height + margin_y - 30
+
+                            #
+                            
                         elif option == "option2":
-                            # Для option2: добавляем фото слева от основного контента
                             additional_photos = []
                             if product.additional_photo1 and os.path.exists(product.additional_photo1.path):
                                 additional_photos.append(product.additional_photo1.path)
@@ -107,32 +143,33 @@ def convert_to_pdf(request):
                                 additional_photos.append(product.additional_photo2.path)
                                 
                             if additional_photos:
-                                # Параметры для дополнительных фото
-                                sidebar_width = int(bg_width * 0.3)  # 30% ширины
-                                sidebar_x = int(bg_width * 0.6)     # 2% отступ слева
-                                content_y = int(bg_height * 0.1)      # 10% отступ сверху
-                                max_photo_height = int(bg_height * 0.32)  # 20% высоты
-                                margin_y = 10  # Отступ между фото
+                                footer_height = int(bg_height * 0.34) 
+                                footer_y = bg_height - footer_height
+                                total_width = bg_width 
+                                num_photos = len(additional_photos)
+                                margin_x = 0
+                                margin_y = 35
                                 
-                                y_pos = content_y
+                                # Жесткие размеры для дополнительных фото
+                                photo_width = ((total_width - (num_photos) * margin_x) // num_photos) - 420
+                                photo_height = footer_height - 2 * margin_y
+                                
+                                x_pos = margin_x + 790
                                 for photo_path in additional_photos:
                                     with Image.open(photo_path) as img:
-                                        # Конвертация RGBA в RGB
-                                        if img.mode == 'RGBA':
-                                            white_bg = Image.new('RGB', img.size, (255, 255, 255))
-                                            white_bg.paste(img, mask=img.split()[3])
-                                            img = white_bg
+                                        if img.mode != 'RGBA':
+                                            img = img.convert('RGBA')
+                                        img = add_rounded_corners(img, radius=15)
                                         
-                                        # Масштабирование с сохранением пропорций
-                                        img.thumbnail((sidebar_width, max_photo_height), Image.LANCZOS)
+                                        # Растягивание до точных размеров
+                                        img = img.resize((photo_width, photo_height), Image.LANCZOS)
                                         
                                         # Позиционирование
-                                        x_pos = sidebar_x + (sidebar_width - img.width) // 2
-                                        bg.paste(img, (x_pos, y_pos))
+                                        y_pos = footer_y  
+                                        bg.paste(img, (x_pos, y_pos), img)
                                         
-                                        y_pos += img.height + margin_y
-                    
-
+                                        x_pos += photo_width + margin_x 
+                            
                         # Сохраняем временное изображение
                         temp_img = os.path.join(temp_dir, f'comp_{product_id}.jpg')
                         bg.convert('RGB').save(temp_img, quality=95)
@@ -147,84 +184,53 @@ def convert_to_pdf(request):
                 price_value = product.price
                 price_str = f"{price_value:,.2f}".replace(',', ' ').replace('.', ',') + " руб."
                 
-                # Различное позиционирование текста для разных вариантов
+                # Различное позиционирование текста
                 if option == 'option1':
-                    # Вариант 1: текст по центру под изображением
-                    # Заголовок
+                    # Вариант 1: текст по центру
                     pdf.set_font('DejaVu', '', 12)
                     pdf.set_xy(50, 40)
                     pdf.cell(297, 15, product.name.upper(), align='C', ln=True)
                     
-                    # Цена
                     pdf.set_font('DejaVu', '', 18)
-                    pdf.set_text_color(220, 50, 50)  # Красный цвет
+                    pdf.set_text_color(220, 50, 50)
                     pdf.set_xy(80, 104)
                     pdf.cell(297, 20, price_str, align='C', ln=True)
-                    pdf.set_text_color(0, 0, 0)  # Возврат к черному
+                    pdf.set_text_color(0, 0, 0)
                     
-                    # Характеристики
                     pdf.set_font('DejaVu', '', 12)
-                    
-                    # # pdf.set_xy(50, 100)
-                    # # pdf.cell(297, 10, f" {product.article}", align='C', ln=True)
-                    
-                    # pdf.set_xy(45, 150)
-                    # # разбей по запятой
-                    # material = product.material
-                    
-                    # pdf.cell(297, 10, f" {product.material}", align='C', ln=True)
-                        # Разбиваем материалы по запятым и выводим каждую в отдельной строке
                     materials = product.material.split(',')
-                    y_pos = 150  # Начальная позиция Y для материалов
-                    
+                    y_pos = 150
                     for material in materials:
-                        material = material.strip()  # Убираем лишние пробелы
+                        material = material.strip()
                         pdf.set_xy(45, y_pos)
                         pdf.cell(297, 10, material, align='C', ln=True)
-                        y_pos += 8  # Отступ между строками
+                        y_pos += 8
                     
                     pdf.set_xy(100, 150)
                     pdf.cell(297, 10, f" {product.dimensions}", align='C', ln=True)
-                    
-                    # pdf.set_xy(100, 180)
-                    # pdf.cell(297, 10, f" {product.color}", align='C', ln=True)
                 else:
                     # Вариант 2: текст слева
-                    # Заголовок
                     pdf.set_font('DejaVu', '', 12)
                     pdf.set_xy(20, 40)
                     pdf.cell(150, 15, product.name.upper(), ln=True)
                     
-                    # Цена
                     pdf.set_font('DejaVu', '', 18)
-                    pdf.set_text_color(220, 50, 50)  # Красный цвет
+                    pdf.set_text_color(220, 50, 50)
                     pdf.set_xy(50, 108)
                     pdf.cell(150, 20, price_str, ln=True)
-                    pdf.set_text_color(0, 0, 0)  # Возврат к черному
+                    pdf.set_text_color(0, 0, 0)
                     
-                    # Характеристики
                     pdf.set_font('DejaVu', '', 12)
-                    
-                    # pdf.set_xy(30, info_y)
-                    # pdf.cell(100, 10, f"{product.article}", ln=True)
-                    
-                    # pdf.set_xy(25, 100)
-                    # pdf.cell(90, 130, f"{product.material}", ln=True)
-                    # Разбиваем материалы по запятым и выводим каждую в отдельной строке
                     materials = product.material.split(',')
-                    y_pos = 160  # Начальная позиция Y для материалов
-                    
+                    y_pos = 160
                     for material in materials:
-                        material = material.strip()  # Убираем лишние пробелы
+                        material = material.strip()
                         pdf.set_xy(17, y_pos)
                         pdf.cell(40, 0, material, align='C', ln=True)
-                        y_pos += 8  # Отступ между строками
+                        y_pos += 8
                     
-                    pdf.set_xy(85,  150)
+                    pdf.set_xy(85, 150)
                     pdf.cell(297, 10, f"{product.dimensions}", ln=True)
-                    
-                    # pdf.set_xy(30, info_y + 30)
-                    # pdf.cell(100, 10, f"{product.color}", ln=True)
                         
             except Exception as e:
                 print(f"Ошибка обработки товара {product_id}: {e}")
@@ -236,9 +242,6 @@ def convert_to_pdf(request):
         pdf_output = pdf.output(dest='S').encode('latin1')
         response.write(pdf_output)
         return response
-    
-
-
 
 def upload_csv(request):
     if request.method == 'POST':
